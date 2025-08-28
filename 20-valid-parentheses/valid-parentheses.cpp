93de8f5ab68a824fa9284d1664a2dad6a45849a3
Vikash Kumar <138879533+vikashritik@users.noreply.github.com>
2025-08-28 14:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (62.29%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-
-        for (char cur : s) {
-            if (!st.empty()) {
-                char last = st.top();
-                if (isPair(last, cur)) {
-                    st.pop();
-                    continue;
-                }
+        stack<char>ch;
+        int i=0;
+        while(i<s.size()){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                ch.push(s[i]);
+                i++;
             }
-            st.push(cur);
+
+               else if(s[i]==')' || s[i]=='}' || s[i]==']' ){
+                    if (ch.empty()) {
+                    return false;
+                } 
+                char top = ch.top();
+                    ch.pop();
+                      if ((s[i] == ')' && top != '(') ||
+                    (s[i] == '}' && top != '{') ||
+                    (s[i] == ']' && top != '[')) {
+                    return false;
+                }
+                i++;
+                }
+             else {
+                return false;
+            }
+       
         }
+          return ch.empty();
 
-        return st.empty();        
     }
-
-private:
-    bool isPair(char last, char cur) {
-        return (last == '(' && cur == ')') ||
-               (last == '{' && cur == '}') ||
-               (last == '[' && cur == ']');
-    }
-};    
+};
