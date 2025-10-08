ab194ae2a8611f962f65b0df589a96c1f7080d8f
Vikash Kumar <138879533+vikashritik@users.noreply.github.com>
2025-10-08 16:44:01 +0530

    Time: 1 ms (27.92%) | Memory: 42.5 MB (27.77%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m;
-        for (int j = 0; j < n; j++) {
-            nums1[i] = nums2[j];
-            i++;
+        int[] arr = new int[m + n];
+        
+        // Copy all elements from nums1 (only first m elements are valid)
+        for (int i = 0; i < m; i++) {
+            arr[i] = nums1[i];
         }
-        Arrays.sort(nums1);
+        
+        // Copy all elements from nums2
+        for (int j = 0; j < n; j++) {
+            arr[m + j] = nums2[j];
+        }
+        
+        // Sort the merged array
+        Arrays.sort(arr);
+        
+        // Copy the sorted array back to nums1
+        for (int i = 0; i < m + n; i++) {
+            nums1[i] = arr[i];
+        }
     }
 }
